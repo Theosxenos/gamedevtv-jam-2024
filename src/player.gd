46b0dc42c0dc1f52898d699d4ec0c8c3e6b76e32
Theosxenos <64856229+Theosxenos@users.noreply.github.com>
2024-05-25 13:48:40 +0200

    Implement basic player functionality
    - Player movement
    - Character sprites
    - Weapon sprites
    - Wall collision

---
+++ src/player.gd
@@ -0,0 +1,10 @@
+extends CharacterBody2D
+
+@export var speed := 45.0
+
+var direction := Vector2.ZERO
+
+func _physics_process(_delta: float) -> void:
+	direction = Input.get_vector("move_left", "move_right", "move_up" ,"move_down")
+	velocity = direction * speed
+	move_and_slide()
